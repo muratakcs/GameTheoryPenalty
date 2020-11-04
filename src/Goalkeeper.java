8b9a6deda01c681a7a89717671e417480be04de6
Murat <muratakcs@gmail.com>
2020-11-04 21:57:38 +0300

    Goalkeeper variables defined.

--- src/Goalkeeper.java
+++ src/Goalkeeper.java
@@ -1,2 +1,24 @@
+import java.util.Random;
+
 public class Goalkeeper {
+    int np; // Number of penalties
+
+    int nl; // Number of left shots
+    int nm; // Number of middle shots
+    int nr; // Number of right shots
+
+    int nlm; // Number of left misses
+    int nmm; // Number of middle misses
+    int nrm; // Number of right misses
+
+    int nls; // Number of left saves
+    int nms; // Number of middle saves
+    int nrs; // Number of right saves
+
+    double probl; // Probability of jumping left
+    double probm; // Probability of staying in the middle
+    double probr; // Probability of jumping right
+
+
+
 }
