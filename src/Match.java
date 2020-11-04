bb41db17593dbaed5078d72b751b8307176e18ca
Murat <muratakcs@gmail.com>
2020-11-04 22:49:42 +0300

    Match process basically implemented, jumping same side saves, otherwise score.

--- src/Match.java
+++ src/Match.java
@@ -1,2 +1,25 @@
 public class Match {
+    Goalkeeper gk;
+    Kicker k;
+    int numpen;
+    public Match(Goalkeeper gkeep, Kicker kick, int numpenalties) {
+        gk = gkeep;
+        k = kick;
+        numpen = numpenalties;
+    }
+
+    public int process() {
+        int kickside;
+        int jumpside;
+        int score = 0;
+        for(int i=0 ;i<numpen; i++) {
+            kickside = k.decide();
+            jumpside = gk.decide();
+            if(kickside!=jumpside) {
+                score++;
+            }
+        }
+        return score;
+    }
+
 }
