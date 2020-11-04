1c39e1a16aeefc66dd5dcebcf656c0dd513df33c
Murat <muratakcs@gmail.com>
2020-11-05 01:24:31 +0300

    Kicker is almost done now.

--- src/Kicker.java
+++ src/Kicker.java
@@ -1,33 +1,29 @@
 import java.util.Random;
 
 public class Kicker {
-
+    String name;
     int[] a; //shooting abilities 0:left 1:middle 2:right
-
     int p; // Number of penalties
-
-    int[] n; // Number of shots 0:left 1:middle 2:right
-
-    int[] m; // Number of misses 0:left 1:middle 2:right
-
-    int[] s; // Number of saves 0:left 1:middle 2:right
-
-
+    int[][][] results; //D1:Kickerside D2:GKSide D3:result(0:miss,1:save,2:goal)
     double[] pr; // Probability of shooting sides 0:left 1:middle 2:right
 
-    public Kicker(int l, int m, int r) {
+    public Kicker(String nm, int l, int m, int r) {
+        name = nm;
         a[0] = l;
         a[1] = m;
         a[2] = r;
         pr[0]=0.40;
         pr[1]=0.20;
         pr[2]=0.40;
+        results = new int[3][3][3];
     }
 
-    public void report(int side_gk, int side_sh, boolean miss, boolean save) {
-
+    public void report(int kside, int gkside, int res) {
+        results[kside][gkside][res]++;
+        // Here, one may want to play with the pr array (probabilities of jumping to which side)
     }
 
+    // Decide method just uses the probabilities calculated above. Do not change this method.
     public int decide() {
         Random rand =new Random();
         int x = rand.nextInt(1000);
