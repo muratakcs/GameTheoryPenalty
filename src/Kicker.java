e01b16a5e7216dd7f7043bfcd866cb386da422af
Murat <muratakcs@gmail.com>
2020-11-04 23:01:48 +0300

    Variables defined as arrays

--- src/Kicker.java
+++ src/Kicker.java
@@ -2,36 +2,26 @@ import java.util.Random;
 
 public class Kicker {
 
-    int abl; // Left shooting ability
-    int abm; // Middle shooting ability
-    int abr; // Right shooting ability
+    int[] a; //shooting abilities 0:left 1:middle 2:right
+
+    int p; // Number of penalties
+
+    int[] n; // Number of shots 0:left 1:middle 2:right
+
+    int[] m; // Number of misses 0:left 1:middle 2:right
+
+    int[] s; // Number of saves 0:left 1:middle 2:right
 
 
-    int np; // Number of penalties
-
-    int nl; // Number of left shots
-    int nm; // Number of middle shots
-    int nr; // Number of right shots
-
-    int nlm; // Number of left misses
-    int nmm; // Number of middle misses
-    int nrm; // Number of right misses
-
-    int nls; // Number of left saves
-    int nms; // Number of middle saves
-    int nrs; // Number of right saves
-
-    double probl; // Probability of shooting left
-    double probm; // Probability of shooting to the middle
-    double probr; // Probability of shooting right
+    double[] pr; // Probability of shooting sides 0:left 1:middle 2:right
 
     public Kicker(int l, int m, int r) {
-        abl = l;
-        abm = m;
-        abr = r;
-        probl=0.40;
-        probr=0.40;
-        probm=0.20;
+        a[0] = l;
+        a[1] = m;
+        a[2] = r;
+        pr[0]=0.40;
+        pr[1]=0.20;
+        pr[2]=0.40;
     }
 
     public void report(int side_gk, int side_sh, boolean miss, boolean save) {
@@ -41,8 +31,8 @@ public class Kicker {
     public int decide() {
         Random rand =new Random();
         int x = rand.nextInt(1000);
-        if(x<1000*probl) return 0; //left;
-        if(x<1000*(probl+probm)) return 1; //middle;
+        if(x<1000*pr[0]) return 0; //left;
+        if(x<1000*(pr[0]+pr[1])) return 1; //middle;
         return 2; //right;
     }
 }
