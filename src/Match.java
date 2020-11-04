b7364c66ddd1237a73eb49c67f52e3927156c559
Murat <muratakcs@gmail.com>
2020-11-05 00:17:49 +0300

    Match class updated.

--- src/Match.java
+++ src/Match.java
@@ -1,10 +1,14 @@
 public class Match {
     Goalkeeper gk;
     Kicker k;
+    double[] probsave;
     int numpen;
     public Match(Goalkeeper gkeep, Kicker kick, int numpenalties) {
         gk = gkeep;
         k = kick;
+        probsave = new double[3];
+        for(int i=0; i<3; i++)
+        probsave[i] = gk.a[i] / (gk.a[i]+k.a[i]);
         numpen = numpenalties;
     }
 
