8dc3e56f4197335005f93407e3d81010b73767c3
Murat <muratakcs@gmail.com>
2020-11-21 23:24:28 +0300

    Re-written according to the new ability vectors.

--- src/PenaltyKick.java
+++ src/PenaltyKick.java
@@ -3,35 +3,37 @@ import java.util.Random;
 
 public class PenaltyKick {
     private Player k;
-    private Player gk;
+    private Player g;
     private Random rand;
     private double[] prscore;
-    private int[] ab;
-    public PenaltyKick(Player kicker, Player gkeep, int[] abilities) {
+    private int[] kab;
+    private int[] gab;
+    public PenaltyKick(Player kicker, Player gkeep, int[] kabi, int[] gabi) {
         k = kicker;
-        gk = gkeep;
-        ab = abilities; //0:kl 1:km 2: kr 3:gl 4:gm 5:gr
+        g = gkeep;
+        kab = kabi; // index:ability  ->  0:kl(kicker left) 1:km 2: kr
+        gab = gabi; // index:ability  ->  0:gl 1:gm 2:gr
 
         rand = new Random();
         prscore = new double[3]; // probability of scoring for each side, given the abilities...
         for(int i=0; i<3; i++)
-            prscore[i] = ab[3+i] / (double)(ab[3+i]+ab[i]);
+            prscore[i] = kab[i] / (double)(gab[i]+kab[i]);
     }
     public int[] shoot() {
         int[] result = new int[3]; // kickside, jumpside, whathappened?0:missed,1:saved,2:scored.
-        result[0] = k.decide(true,gk); // kicker decides
-        result[1] = gk.decide(false,k); // goalie decides
-        int r = rand.nextInt(100);
-        if(r>ab[result[0]]) { // if rand is above the kickers shooting ability, miss...
+        result[0] = k.decide(true, g); // kicker decides
+        result[1] = g.decide(false,k); // goalie decides
+        int r = rand.nextInt(100)+1;
+        if(r>kab[result[0]]) { // if rand is above the kickers shooting ability, miss...
             result[2]=0; // Kicker misses the goal
             return result;
         }
         // if kicker can kick the ball at least towards the net...
         double probgoal = prscore[result[0]]; // this is the default probability of scoring at that side.
-        r = rand.nextInt(100); // randomize again to decide save or not
+        r = rand.nextInt(100)+1; // randomize again to decide save or not
 
-        //This line can be implemented better.  When shot right, jump left should be worse than staying in the middle.
         if(result[0]!=result[1]) probgoal = (9+probgoal)/10.0; // If gk jumps towards a wrong direction, saving probability drops substantially
+        if((result[0]-result[1])*(result[0]-result[1])==4) probgoal = (9+probgoal)/10.0; // If gk jumps towards opposite direction, saving probability drops even more
         if(r<100* probgoal)
             result[2] = 2; // Same side but kicker scores.
         else
@@ -44,19 +46,23 @@ public class PenaltyKick {
 
         Player k = new Player("Baggio");
         Player gk = new Player("Zubizarreta");
-        int[] ab = {95,80,90,80,90,70};
+        int[] abk = {95,80,90};
+        int[] abg = {80,90,70};
         PenaltyKick pk;
         int[][][] results = new int[3][3][3];
         int[] res;
+        int[] c={0,0,0};
+
         for(int i=0; i<1000000; i++) {
-            pk = new PenaltyKick(k, gk, ab);
+            pk = new PenaltyKick(k, gk, abk, abg);
             res = pk.shoot();
             results[res[0]][res[1]][res[2]]++;
+            c[res[0]]++;
         }
         for(int i=0; i<3; i++) {
-            System.out.println("Kicker shoots to "+i);
+            System.out.println("Kicker "+ k.name +" shoots to "+i+ "    "+c[i]+" times");
             for (int j = 0; j < 3; j++) {
-                System.out.println("Goalie jumps to "+j);
+                System.out.println("Goalie "+ gk.name +" jumps to "+j);
 
                 System.out.println("MISSES | SAVES | GOALS");
                 System.out.println(Arrays.toString(results[i][j]));
